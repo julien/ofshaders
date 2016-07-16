fdd4e6c2b4b3ff44705ab55a8e9d55d1a861e74e
Julien C <jcastelain@gmail.com>
2016-07-16 20:43:17 +0200

    remove comments

--- bin/data/blobs.glsl
+++ bin/data/blobs.glsl
@@ -7,16 +7,6 @@ uniform float u_time;
 
 void main() {
 
-    // vec2 st = gl_FragCoord.xy / u_resolution.xy;
-    // vec3 color = vec3(0.0);
-    //
-    // float left = step(0.1, st.x);
-    // float bottom = step(0.1, st.y);
-    //
-    // color = vec3(left * bottom);
-    //
-    // gl_FragColor = vec4(color, 1.0);
-
     vec2 p = -1.0 + 2.0 * gl_FragCoord.xy / u_resolution.xy;
 
     vec2 v = vec2(2, -.5);
